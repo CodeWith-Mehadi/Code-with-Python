91d8bb98cf8a8d552c3402677b1a81122c7aea6b
Mehedi Hassan <mehadi.cse01@gmail.com>
2024-11-22 19:22:45 +0600

    Added BMI calculator program

---
+++ BMI.py
@@ -0,0 +1,9 @@
+#Welcome to Code With Mehadi...
+print("BMI test :")
+Height = float (input("Enter your height on mitter : "))
+print("Your height is :", Height)
+Weight = float (input("Enter your weight on KG : "))
+print("Your weight is :", Weight)
+
+BMI = Weight / (Height*Height)
+print(f" \nSo, Your BMI is : {BMI:.2f}")
